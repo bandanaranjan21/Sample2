68184cca133a3f1275ef98b7784882c4913440f0
John Doe <johndoe@example.com>
2023-04-20 19:53:26 -0500

    python file

---
+++ Sample.py
@@ -0,0 +1,127 @@
+import time
+
+#print("Hello")
+#print("I Love ATHARV") 
+
+# my first program
+#x = 5
+#y ="BROther" 
+#print( str(x) + y) 
+#height = 205.5
+#print(type(height))
+
+name = "Bro"
+#age = 21
+#attractive = True
+#print(attractive)
+#print(len(name))
+
+#print(name.find("o"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("o"))
+
+##print(name.replace("r","l"))
+
+#print(name*3) # print 3 times
+
+#Type Casting
+
+#x = 1 #int
+#y = 2.0 #float
+#z = "3" #string
+
+#z=int(z)
+
+#print(x)
+#print(y)
+#print(z*3)
+
+#name = input("What is your name : ")
+#print("Hello: " + name)
+
+#age = int(input("How old are you ?"))
+
+#age = age +1
+
+#print("Hello  " + name)
+#print("you are  "+ str(age) + "  Year old")
+
+#Slicing
+
+# name = "Bandana Ranjan"
+
+# first_name = name[0:7]
+# last_name = name[8:15]
+# print(first_name)
+# print(last_name)
+
+# temp = int(input ("what is the temperature outside? "))
+
+# if temp < 0 :
+
+#     print("Temperature is  bad today")
+
+# elif temp > 30 :
+
+#     print("temperature os hot today")
+
+# else :
+
+#     print("nice weather")
+
+# for i in range(10) :
+#     print(i+1)
+
+# for i in range(50,100+1, 2):
+#     print(i)
+
+# for i in "Bandana Ranjan":
+#     print(i)
+
+# for seconds in range(10,0,-1):
+#  print(seconds)
+#  time.sleep(1)
+# print("happy new year !")
+
+######### nested loop
+
+# rows = int (input("how many rows? :  "))
+# columns = int (input("how many columns?: "))
+
+# symbol = input("enter a symbol to use")
+
+# for i in range(rows):
+#     for j in range (columns):
+#         print(symbol,end="")
+#     print()    
+
+# while True:
+#     name = input("enter your name:  ")
+#     if  name != "":
+#      break
+
+# phone_number ="234-234-2344"
+
+# for i in phone_number:
+#     if i == "-":
+#         continue
+#     print(i, end="")
+
+
+# for i in range(1,15):
+#     if i == 13:
+#         pass
+#     else:
+#         print(i)
+
+while 1==1:
+    print("well done")
+  
+
+
+
+
